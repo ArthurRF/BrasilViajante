c043359ccd2a8738a00b6f6f8c56b6d6d3c2bec6
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-12-13 22:44:26 -0300

    Config envs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', async (req: Request, res: Response): Promise<Response> => {
-  return res.send('Hello World!');
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json(err.message);
+  }
+
+  return res.status(500).json(err);
 })
 
-app.listen(5555, () => console.log('Server is running on port 5555!'));
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ message: `Rodando na porta ${process.env.APP_PORT}` });
+})
+
+app.listen(process.env.APP_PORT, () => console.log(`Server is running on port ${process.env.APP_PORT}!`));
